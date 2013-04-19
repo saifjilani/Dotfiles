b6c5347298076b3ce1b7f5091e42897a3cfba42c
Celso Dantas <celsodantas@gmail.com>
2013-04-19 18:02:02 -0500

    updates to gitconfig

--- alias.sh
+++ alias.sh
@@ -1,4 +1,4 @@
 alias glog="git log --graph --pretty='format:%C(yellow)%h%C(cyan)%d%Creset %s => %C(green)%an%C(white), %C(red)%ar%Creset'"
-alias gca="git commit -a"
+alias gca="git add -A && git commit"
 alias top="top -o cpu"
 alias e='subl . &'

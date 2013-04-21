81d023819f85bcefdd153ef2beeeeb8ed5c24af9
Celso Dantas <celsodantas@gmail.com>
2013-04-20 22:16:10 -0500

    one more fix to gca

--- alias.sh
+++ alias.sh
@@ -1,5 +1,5 @@
 alias glog="git log --graph --pretty='format:%C(yellow)%h%C(cyan)%d%Creset %s => %C(green)%an%C(white), %C(red)%ar%Creset'"
-alias gca="git commit -a"
+alias gca="git add --all && git commit -a"
 alias top="top -o cpu"
 alias e='subl . &'
 alias c='cd'

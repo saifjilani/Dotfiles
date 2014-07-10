28a8031ec7768b75d98e741047dd4ef2e01821ad
Celso Dantas <celsodantas@gmail.com>
2014-07-09 20:11:09 -0400

    organizing

--- export.sh
+++ export.sh
@@ -1,4 +1,5 @@
 export ANT_ARGS='-logger org.apache.tools.ant.listener.AnsiColorLogger'
-export PATH="/usr/local/share/npm/bin:$PATH"
-export SPR_APP_URL='celsodantas-shopifyapps.fwd.wf'
+export PATH="~/bin:$PATH"
 
+export GW2ORES_USER=homer
+export SPR_APP_URL='celsodantas-shopifyapps.fwd.wf'
